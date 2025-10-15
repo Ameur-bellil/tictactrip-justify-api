e3d9582a8684659ef0c5710cfa397ebd48232fcc
Ameur-bellil <ameur.bellil@eniso.u-sousse.tn>
2025-10-15 19:29:59 +0100

    [ADDED] Add Swagger documentation for Justify API endpoint

--- src/routes/justify.route.ts
+++ src/routes/justify.route.ts
@@ -4,6 +4,55 @@ import { authenticate } from "../middlewares/auth.middleware";
 
 const justifyRouter = Router();
 
+/**
+ * @swagger
+ * /api/justify:
+ *   post:
+ *     summary: Justifie un texte
+ *     description: |
+ *       Prend un texte brut et le justifie à une largeur fixe (80 caractères par ligne).
+ *       - Nécessite un token JWT valide (envoyé dans le header `Authorization: Bearer <token>`).
+ *       - La dernière ligne du texte n’est **pas justifiée**.
+ *     tags:
+ *       - Justify
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         text/plain:
+ *           schema:
+ *             type: string
+ *             example: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum."
+ *     responses:
+ *       200:
+ *         description: Texte justifié avec des espaces distribués uniformément.
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               example: "Lorem  ipsum  dolor  sit  amet,  consectetur  adipiscing  elit. Vestibulum nec odio ipsum."
+ *       400:
+ *         description: Body vide ou invalide.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "No text provided or invalid format"
+ *       401:
+ *         description: Token manquant ou invalide.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Missing token"
+ */
 justifyRouter.post("/justify", authenticate, JustifyController.justify);
 
 export default justifyRouter;
