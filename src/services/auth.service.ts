72d13b482e66b5bd3446850f5d7d5c30bd9270c2
Ameur-bellil <ameur.bellil@eniso.u-sousse.tn>
2025-10-15 19:00:13 +0100

    [ADDED] Add verifyToken method to AuthService class

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,4 +6,11 @@ export class AuthService {
         return jwt.sign({ email }, env.JWT_SECRET!, { expiresIn: "24h" });
     }
 
+    static verifyToken(token: string): JwtPayload | null {
+        try {
+            return jwt.verify(token, env.JWT_SECRET!) as JwtPayload;
+        } catch {
+            return null;
+        }
+    }
 }
