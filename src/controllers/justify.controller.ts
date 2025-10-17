ec8e03641934abbbf2df054c45026ec93f5e0962
Ameur-bellil <ameur.bellil@eniso.u-sousse.tn>
2025-10-17 01:59:06 +0100

    [ADDED] refactor justifyService

--- src/controllers/justify.controller.ts
+++ src/controllers/justify.controller.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from "express";
 import { JustifyService } from "../services/justify.service";
 
-
 const justifyService = new JustifyService(80);
-export class JustifyController {
-    static justify(req: Request, res: Response) {
+
+export const JustifyController = {
+    async justify(req: Request, res: Response) {
         const text = req.body;
 
         if (!text || typeof text !== "string") {
             return res.status(400).json({ error: "No text provided or invalid format" });
         }
 
-        const justifiedText = justifyService.justifyText(text);
-        res.send(justifiedText);
-    }
-}
+        const justifiedText =  await justifyService.justifyText(text);
+
+        res.status(200).send(justifiedText);
+    },
+};
